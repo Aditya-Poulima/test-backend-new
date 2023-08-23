b0ca4be53104a95112509d996ae254407accd7fb
Aditya-Poulima <rajguru.aditya.25@gmail.com>
2023-08-23 12:19:46 +0530

    Change src file name

---
+++ data.js
@@ -0,0 +1,46 @@
+const dummyUsers = [
+  {
+    id: 1,
+    name: "Alice",
+    age: 23,
+    married: false,
+    comment: "Hello, I am Alice.",
+  },
+  {
+    id: 2,
+    name: "Bob",
+    age: 25,
+    married: false,
+    comment: "Hello, I am Bob.",
+  },
+  {
+    id: 3,
+    name: "Chris",
+    age: 21,
+    married: false,
+    comment: "Hello, I am Chris.",
+  },
+  {
+    id: 4,
+    name: "David",
+    age: 27,
+    married: true,
+    comment: "Hello, I am David.",
+  },
+  {
+    id: 5,
+    name: "Ellen",
+    age: 24,
+    married: true,
+    comment: "Hello, I am Ellen.",
+  },
+  {
+    id: 6,
+    name: "Frank",
+    age: 26,
+    married: true,
+    comment: "Hello, I am Frank.",
+  },
+];
+
+export default dummyUsers;
---
+++ testFile.js
@@ -0,0 +1,39 @@
+import dummyUsers from "./data.js";
+
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Welcome to Rewards VPN");
+  res.end();
+});
+
+app.get("/users", (req, res) => {
+  res.status(200);
+  res.send(dummyUsers);
+  res.end();
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = dummyUsers.find((user) => user.id === Number(id));
+  if (user) {
+    res.status(200);
+    res.send(user);
+  } else {
+    res.status(404);
+    res.send({ message: "User not found." });
+  }
+  res.end();
+});
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
